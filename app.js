310d6498a46f462307b820563ffcf20ab0daada4
Gurdeep Singh <gurdeep.eec70@gmail.com>
2016-04-12 00:14:27 +0530

    added service

--- app.js
+++ app.js
@@ -3,21 +3,29 @@ var weatherApp = angular.module('weatherApp',['ngRoute','ngResource']);
 
 //Routes
 weatherApp.config(function($routeProvider){
- $routeProvider
- .when('/',{
-   templateUrl:'pages/home.html',
-   controller:'homeController'
- })
- .when('/forecast',{
-   templateUrl:'pages/forecast.html',
-   controller:'forecastController'
- })
+  $routeProvider
+  .when('/',{
+    templateUrl:'pages/home.html',
+    controller:'homeController'
+  })
+  .when('/forecast',{
+    templateUrl:'pages/forecast.html',
+    controller:'forecastController'
+  })
 });
 
- //controller
- weatherApp.controller('homeController',[$scope,function($scope){
+//services
+weatherApp.service('cityService',function(){
+  this.city = "Jammu and Kashmir, J&K";
+})
 
- }]);
- weatherApp.controller('forecastController',[$scope,function($scope){
-
- }]);
+//controller
+weatherApp.controller('homeController',['$scope','cityService',function($scope,cityService){
+  $scope.city = cityService.city;
+  $scope.$watch('city',function(){
+    cityService.city = $scope.city;
+  });
+}]);
+weatherApp.controller('forecastController',['$scope','cityService',function($scope,cityService){
+  $scope.city = cityService.city;
+}]);
